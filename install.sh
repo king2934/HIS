e2cf6824956cba466e4a10733cf18009c4a48c7e
king2934 <king2934@gmail.com>
2020-07-26 22:02:29 +0800

    linux

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ cp -r release/* /usr/local/lanhuihis/
 echo "[Desktop Entry]">/home/`whoami`/桌面/LanHuiHIS.desktop
 echo "Encoding=UTF-8">>/home/`whoami`/桌面/LanHuiHIS.desktop
 echo "Name=蓝徽医院信息系统">>/home/`whoami`/桌面/LanHuiHIS.desktop
-echo "Type=Application">>/home/`whoami`/桌面/LanHuiHIS.desktop
-echo "Terminal=false">>/home/`whoami`/桌面/LanHuiHIS.desktop
 echo "Exec=/usr/local/lanhuihis/start.sh">>/home/`whoami`/桌面/LanHuiHIS.desktop
+echo "Type=Application">>/home/`whoami`/桌面/LanHuiHIS.desktop
 echo "Icon=/usr/local/lanhuihis/images/image_title.png">>/home/`whoami`/桌面/LanHuiHIS.desktop
+echo "Terminal=false">>/home/`whoami`/桌面/LanHuiHIS.desktop
 
+sudo a+x /home/`whoami`/桌面/LanHuiHIS.desktop
